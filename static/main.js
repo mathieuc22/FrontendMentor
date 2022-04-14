e8998a84ac672ad7fbb0b857c98ebb7d0af2e6e3
Mathieu <mathieu.collet@gmail.com>
2022-04-14 11:15:48 +0200

    refactor and mobile height correction

--- static/main.js
+++ static/main.js
@@ -4,10 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".mobile-nav-toggle").addEventListener("click", (event) => {
     if (document.body.classList.contains("modal-open")) {
         document.body.classList.remove("modal-open")
+        document.querySelector("header").classList.remove("modal-open")
         document.querySelector(".mobile-nav").classList.remove("mobile-nav--show")
         document.querySelector(".mobile-nav-toggle").src="./images/icon-hamburger.svg"
     } else {
         document.body.classList.add("modal-open")
+        document.querySelector("header").classList.add("modal-open")
         document.querySelector(".mobile-nav").classList.add("mobile-nav--show")
         document.querySelector(".mobile-nav-toggle").src="./images/icon-close.svg"
         document.querySelector(".mobile-nav").classList.add("animation");
