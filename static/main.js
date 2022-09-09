ef9173b38976fbdccf77112081bf50039aee7c4b
Mathieu <mathieu.collet@gmail.com>
2022-09-09 14:21:13 +0200

    Mobile menu animation +  hover effect footer links

--- static/main.js
+++ static/main.js
@@ -1,17 +1,22 @@
 // Main
 document.addEventListener("DOMContentLoaded", function () {
-  const navbar = document.querySelector(".header__links")
-  const toggle = document.querySelector(".header___hamburger")
+  const navbar = document.querySelector(".header__links");
+  const toggle = document.querySelector(".header___hamburger");
   toggle.addEventListener("click", (event) => {
     if (navbar.classList.contains("display")) {
-      navbar.classList.remove("display")
-  } else {
-    navbar.classList.add("display")
+      navbar.classList.add("fadeout");
+      navbar.classList.remove("display");
       // After the animation, reset everything to its default state
-      // navbar.addEventListener('animationend', function(event){
-      //     navbar.classList.remove("animation");
-      //  });
-  }
+      navbar.addEventListener("animationend", function (event) {
+        navbar.classList.remove("fadeout");
+      });
+    } else {
+      navbar.classList.add("display");
+      navbar.classList.add("fadein");
+      // After the animation, reset everything to its default state
+      navbar.addEventListener('animationend', function(event){
+          navbar.classList.remove("fadein");
+       });
+    }
   });
 });
-
