9c52536d2ef9fe665d4dda9bb70cc93d77d81405
Mathieu <mathieu.collet@gmail.com>
2022-09-12 09:36:54 +0200

    Update and deploy under FrontendMentor

--- ecommerce-product-page-main/vite.config.js
+++ ecommerce-product-page-main/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ecommerce-product-page-main/',
+  base: '/FrontendMentor/ecommerce-product-page-main/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
