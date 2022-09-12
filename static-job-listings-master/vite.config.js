bab82abdf24611ae1614a5bad9766c640a718534
Mathieu <mathieu.collet@gmail.com>
2022-09-12 10:52:46 +0200

    Correction for vite.config

--- static-job-listings-master/vite.config.js
+++ static-job-listings-master/vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/static-job-listings-master/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
